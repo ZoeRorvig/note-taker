28a753513e0bfdc7becbfa5ffe967fae66c2fb86
Zoe Rorvig <zrorvig@gmail.com>
2023-01-05 20:33:19 -0500

    added get to display home page

--- server.js
+++ server.js
@@ -13,8 +13,15 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.send('Note Taker');
-});
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
-app.listen(PORT);
+// app.get('/', (req, res) => {
+//   res.send('Note Taker');
+// });
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
