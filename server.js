bf19dbda1cbda56cb3a49abe1799487ba07b2e5e
Zoe Rorvig <zrorvig@gmail.com>
2023-01-05 20:25:59 -0500

    added middleware

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const uuid = require('./helpers/uuid');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
