4df7078d8da8319b3b7b7199121745c5bd004f6f
Zoe Rorvig <zrorvig@gmail.com>
2023-01-05 23:49:09 -0500

    added new post route for new notes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const tasks = require('./db/db.json');
+const notes = require('./db/db.json');
 const uuid = require('./helpers/uuid');
 
 // Initialize express app
@@ -25,12 +25,30 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-
-
-// GET Route for retrieving all the tasks
+// GET Route for retrieving all the notes
 app.get('/api/notes', (req, res) => {
-  console.info(`${req.method} request received for tasks`);
-  res.json(tasks);
+  console.info(`${req.method} request received for notes`);
+  res.json(notes);
+});
+
+// POST Route for a new note
+app.post('/api/notes', (req, res) => {
+  console.info(`${req.method} request received to add a note`);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      task_id: uuid(),
+    };
+
+    res.json(newNote);
+    notes.push(newNote);
+    fs.writeFile(path.join(__dirname, "/db/db.json"), JSON.stringify(notes), (err) =>
+      err ? console.error(err) : console.info(`\nData written to db.json`));
+  }
 });
 
 app.listen(PORT, () =>
