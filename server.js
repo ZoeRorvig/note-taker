45585f2029105f1e13c4e5e53b6bac624c3b8683
Zoe Rorvig <zrorvig@gmail.com>
2023-01-05 20:25:04 -0500

    added requires to server.js

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
-const PORT = process.env.PORT || 3001;
+const path = require('path');
+const fs = require('fs');
+const database = require('./db/db.json');
+const uuid = require('./helpers/uuid');
 
+const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.send('Note Taker');
